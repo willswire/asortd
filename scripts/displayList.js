37bf30c1067285bf698a668633efe138fd6c67b1
Connor Onweller <onweller@udel.edu>
2019-09-10 10:15:42 -0400

    Added movement animation for updating block position

--- scripts/displayList.js
+++ scripts/displayList.js
@@ -1,13 +1,15 @@
 // Class: Block
 // Attributes:
 //      Number Height: the height of the block
-function Block(height) {
+//      String id: name identifying block
+function Block(id, height) {
+    this.id = 'block' + id;
     this.height = height;
 }
 
 // BLOCKS: List of items being sorted, where each element represent the block
 //      height
-var BLOCKS = [new Block(5), new Block(10), new Block(2), new Block(8)];
+var BLOCKS = [new Block(1, 5), new Block(2, 10), new Block(3, 2), new Block(4, 8)];
 // BLOCK_WIDTH: Width in pixels for displaying blocks
 var BLOCK_WIDTH = 20
 // BLOCK_OFFSET: The space between blocks in pixels
@@ -24,7 +26,7 @@ var BLOCK_HEIGHT_COEF = 10
 // Returns:
 //      The html object representing the block in a string
 function CreateBlock(block, idx) {
-    var block = '<div class="sort_block" ' +
+    var block = '<div class="sort_block" id="' + block.id + '" ' +
         'style="position:absolute;bottom:0px;left:' + 
         (BLOCK_OFFSET*idx) + 'px;background:black;width:' + BLOCK_WIDTH +
         'px;height:' + (BLOCK_HEIGHT_COEF*block.height) + 'px;">' +
@@ -33,19 +35,39 @@ function CreateBlock(block, idx) {
     
 }
 
+// DrawBlocks :: String, [Block]  -> None
+// Description: Draws the given blocks being displayed in the given container
+// Consumes:
+//      String containerID: the name of the html object in which blocks will be
+//          displayed
+//      [Number] blockList: the list representation of the blocks being sorted
+function DrawBlocks(containerID, blockList) {
+    $(containerID).html('');
+    blockList.forEach(function(block,idx) {
+        $(containerID).append(CreateBlock(block,idx));
+    });
+}
+
 // UpdateBlocks :: String, [Block]  -> None
-// Description: Updates the given blocks being displayed in the given container
+// Description: Animates and updates the given blocks being displayed in the
+//      given container
 // Consumes:
 //      String containerID: the name of the html object in which blocks will be
 //          displayed
 //      [Number] blockList: the list representation of the blocks being sorted
 function UpdateBlocks(containerID, blockList) {
     blockList.forEach(function(block,idx) {
-        $(containerID).append(CreateBlock(block,idx));
+        $('#' + block.id).animate({left: (BLOCK_OFFSET * idx) + 'px'});
     });
 }
 
 // Executed when page is loaded
 $(function() {
-    UpdateBlocks('#sorting_container', BLOCKS);
+    DrawBlocks('#sorting_container', BLOCKS);
+    
+    // Just for testing
+    $('#sorting_container').click(function(event){
+        BLOCKS.reverse();
+        UpdateBlocks('#sorting_container', BLOCKS);
+    })
 });
