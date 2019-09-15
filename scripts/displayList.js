0b9839376a0c32e0bc36e5951604043f5b572638
Scott Benton <scott.ma.benton@gmail.com>
2019-09-15 00:36:25 -0400

    Added functionality to the speed slider and made the start/pause button into a reset button whenever the array is sorted.

--- scripts/displayList.js
+++ scripts/displayList.js
@@ -1,10 +1,11 @@
-const BLOCK_WIDTH = 10
-const BLOCK_YPOS_OFFSET = 15
-const MAX_BLOCK_HEIGHT_PERCENT = 75
-const ANIMATION_SPEED = 100
-const BLOCK_COLOR = '#00a0df'
-const BLOCK_HIGHLIGHT = '#f5cd5d'
+const BLOCK_WIDTH = 10;
+const BLOCK_YPOS_OFFSET = 15;
+const MAX_BLOCK_HEIGHT_PERCENT = 75;
+const BLOCK_COLOR = '#00a0df';
+const BLOCK_HIGHLIGHT = '#f5cd5d';
 const NUM_BLOCKS = 20;
+
+const DEFAULT_DELAY_VALUE = 100;
 
 // Class: Block
 // Attributes:
@@ -44,13 +45,13 @@ function drawBlocks(containerID, blockArr) {
     });
 }
 
-function redrawBlocks(currentArr, nextArr) {
+function redrawBlocks(currentArr, nextArr, animation_speed) {
     nextArr.forEach(function (block, idx) {
         if (block != currentArr[idx]) {
             $('#block' + block.id).animate({
                 left: (block.width * idx) + '%'
             }, {
-                duration: ANIMATION_SPEED,
+                duration: animation_speed,
                 easing: 'linear',
                 start: function () {
                     $('#block' + block.id).css({
@@ -66,7 +67,7 @@ function redrawBlocks(currentArr, nextArr) {
                 }
             });
         } else {
-            $('#block' + block.id).delay(ANIMATION_SPEED);
+            $('#block' + block.id).delay(animation_speed);
         }
     });
 }
@@ -92,15 +93,15 @@ function bubbleSort(blockArr) {
     return steps;
 }
 
-function drawNextStep(steps, currentStep) {
+function drawNextStep(steps, currentStep, animation_speed) {
     if (currentStep >= 0 && currentStep + 1 < steps.length) {
-        redrawBlocks(steps[currentStep], steps[currentStep + 1]);
+        redrawBlocks(steps[currentStep], steps[currentStep + 1], animation_speed);
     }
 }
 
-function drawPreviousStep(steps, currentStep) {
+function drawPreviousStep(steps, currentStep, animation_speed) {
     if (currentStep < steps.length && currentStep - 1 >= 0) {
-        redrawBlocks(steps[currentStep], steps[currentStep - 1]);
+        redrawBlocks(steps[currentStep], steps[currentStep - 1], animation_speed);
     }
 }
 
@@ -117,41 +118,67 @@ $(function () {
     var currentIndex = 0;
     var paused = true;
 
+    var isReset = false;
+
+    var animation_speed = DEFAULT_DELAY_VALUE;
+    $("#slider_range").val(DEFAULT_DELAY_VALUE);
+
     // Just for testing
     $("#sort_button").click(async function (event) {
-        if (paused) {
+        if (paused && !isReset) {
             $("#sort_button").html('PAUSE');
             paused = false;
             for (var i = currentIndex; i < steps.length; i++) {
-                drawNextStep(steps, i);
+                drawNextStep(steps, i, animation_speed);
                 currentIndex++;
-                await sleep(ANIMATION_SPEED);
+                await sleep(animation_speed);
                 if (paused) {
                     break;
                 }
             }
             paused = true;
-            $("#sort_button").html('START');
+            if (currentIndex === steps.length) {
+                $("#sort_button").html('RESET');
+                isReset = true;
+            }
         }
-        else {
+        else if (isReset) {
+            isReset = false;
+            currentIndex = 0;
+            redrawBlocks(steps[steps.length-1], steps[0], animation_speed);
+            $("#sort_button").html('START');
+        } else {
             $("#sort_button").html('START');
             paused = true;
         }
     })
 
     $("#prev_button").click(function () {
-        drawPreviousStep(steps, currentIndex);
+        drawPreviousStep(steps, currentIndex, animation_speed);
         if (currentIndex > 0) {
             currentIndex--;
+        }
+        if (isReset) {
+            $("#sort_button").html('START');
+            isReset = false;
         }
     });
 
     $("#next_button").click(function () {
-        drawNextStep(steps, currentIndex);
+        drawNextStep(steps, currentIndex, animation_speed);
         if (currentIndex < steps.length) {
             currentIndex++;
         }
+        if (currentIndex === steps.length) {
+            $("#sort_button").html('RESET');
+            isReset = true;
+        }
     });
+
+    $("#slider_range").change(function () {
+        animation_speed = parseInt($("#slider_range").val());
+        console.log(animation_speed);
+    })
 
 });
 
